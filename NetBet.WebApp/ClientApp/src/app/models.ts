b0de59c6b183d80da7ce61c06db0e52ecb593aa0
Dustin Hartrick <gammatronninethousand@gmail.com>
2019-06-24 23:02:02 -0400

    messed around with the stupid navbar css, changed TS interfaces to classes

--- NetBet.WebApp/ClientApp/src/app/models.ts
+++ NetBet.WebApp/ClientApp/src/app/models.ts
@@ -1,32 +1,60 @@
-﻿export interface Season {
-    startingCash: string;
-    endTime: Date;
-    startTime: Date;
-    name: string;
-    id: number;
-    minimumCash: number;
-    maxParlaySize: number;
+﻿export class Season {
+    public startingCash: number;
+    public endTime: Date;
+    public startTime: Date;
+    public name: string;
+    public id: number;
+    public minimumCash: number;
+    public maxParlaySize: number;
+    constructor() {
+        this.startingCash = 0;
+        this.endTime = new Date();
+        this.startTime = new Date();
+        this.name = '';
+        this.id = 0;
+        this.minimumCash = 0;
+        this.maxParlaySize = 0;
+    }
 }
 
-export interface SeasonPlayer {
+export class SeasonPlayer {
     seasonID: number;
     playerID: number;
     seasonName: string;
     playerName: string;
     minimumCash: number;
     currentCash: number;
+    constructor() {
+        this.seasonID = 0;
+        this.playerID = 0;
+        this.seasonName = '';
+        this.playerName = '';
+        this.minimumCash = 0;
+        this.currentCash = 0;
+    }
 }
 
-export interface NbEvent {
+export class NbEvent {
     id: number;
     seasonID: number;
     name: string
     startTime: Date;
+    constructor() {
+        this.id = 0;
+        this.seasonID = 0;
+        this.name = '';
+        this.startTime = new Date();
+    }
 }
 
 
-export interface FullSeason {
-    season: Season;
-    players: SeasonPlayer[];
-    events: NbEvent[]
+export class FullSeason {
+    public season: Season;
+    public players: SeasonPlayer[];
+    public events: NbEvent[]
+    constructor() {
+        this.events = [];
+        this.players = [];
+        this.season = new Season();
+    }
 }
--- NetBet.WebApp/ClientApp/src/app/season-detail/season-detail.component.ts
+++ NetBet.WebApp/ClientApp/src/app/season-detail/season-detail.component.ts
@@ -9,19 +9,20 @@ import { Season, SeasonPlayer, NbEvent, FullSeason } from "../models";
   styleUrls: ['./season-detail.component.css']
 })
 export class SeasonDetailComponent implements OnInit {
-    public seasonDetail: FullSeason;
+    public seasonDetail = new FullSeason();
 
     constructor(private route: ActivatedRoute, public http: HttpClient) {
-    }
-
-    ngOnInit() {
         let id = this.route.snapshot.paramMap.get('id');
-        this.http.get<FullSeason>('/api/Season/GetFullSeason/' + id ).subscribe(result => {
+        this.http.get<FullSeason>('/api/Season/GetFullSeason/' + id).subscribe(result => {
             let sortedPlayers = result.players.sort((n1, n2) => n1.currentCash - n2.currentCash).reverse();
             result.players = sortedPlayers;
             this.seasonDetail = result;
             console.log(this.seasonDetail);
         }, error => console.error(error));
+    }
+
+    ngOnInit() {
+
     }
 
 }
