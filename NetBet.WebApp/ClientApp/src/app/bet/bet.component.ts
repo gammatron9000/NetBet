89a85f1626cf060cfcd585a379cb156ce7459544
Dustin Hartrick <gammatronninethousand@gmail.com>
2019-09-04 22:39:10 -0400

    bunch of updates to the bet screen

--- NetBet.WebApp/ClientApp/src/app/bet/bet.component.ts
+++ NetBet.WebApp/ClientApp/src/app/bet/bet.component.ts
@@ -3,7 +3,8 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { Player, PrettyBet, PrettyMatch, EventWithPrettyMatches, NbEvent, SeasonPlayer } from "../models";
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { makeStateKey } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-bet',
@@ -13,26 +14,43 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 export class BetComponent implements OnInit {
     private eventID = 0;
     public faPlus = faPlus;
+    public faTimes = faTimes;
     public allPlayers: SeasonPlayer[] = [];
     public selectedPlayer = new SeasonPlayer();
     public allBetsForEvent: PrettyBet[] = [];
     public matches: PrettyMatch[] = [];
     public evnt: NbEvent = new NbEvent();
     public betslip: PrettyBet[] = [];
+    public currentBetsForPlayer: PrettyBet[] = [];
     public isParlay = false;
+    public parlayStake = 0.0;
 
     constructor(private route: ActivatedRoute, public http: HttpClient, private toastr: ToastrService) {
         this.eventID = Number(this.route.snapshot.paramMap.get('eventid'));
         this.getEventAndMatches();
-        this.getBets();
     }
 
     ngOnInit() { }
 
+    getBets() {
+        this.http.get<PrettyBet[]>('/api/bet/GetPrettyBetsForEvent/' + this.eventID).subscribe(result => {
+            this.allBetsForEvent = result;
+            console.log('bets', result);
+            let currentPlayer = this.selectedPlayer; // makes this visible inside the following filter function
+            this.currentBetsForPlayer = this.allBetsForEvent.filter(function (bet) {
+                return bet.playerID === currentPlayer.playerID;
+            });
+        }, error => console.error('error getting bets: ', error));
+    }
+
     getSeasonPlayers(seasonID) {
         this.http.get<SeasonPlayer[]>('/api/Player/GetPlayersForSeason/' + seasonID).subscribe(result => {
             this.allPlayers = result;
+            if (this.allPlayers.length > 0) {
+                this.selectedPlayer = this.allPlayers[0];
+            }
             console.log('players', result);
+            this.getBets();
         }, error => console.error('error getting players: ', error));
     }
     
@@ -47,11 +65,15 @@ export class BetComponent implements OnInit {
         }, error => console.error('error getting event and matches: ', error));
     }
 
-    getBets() {
-        this.http.get<PrettyBet[]>('/api/bet/GetPrettyBetsForEvent/' + this.eventID).subscribe(result => {
-            this.allBetsForEvent = result;
-            console.log('bets', result);
-        }, error => console.error('error getting bets: ', error));
+    selectedPlayerChanged() {
+        this.isParlay = false;
+        this.parlayStake = 0.0;
+        this.betslip = [];
+        this.currentBetsForPlayer = [];
+        let newPlayer = this.selectedPlayer;
+        this.currentBetsForPlayer = this.allBetsForEvent.filter(function (bet) {
+            return bet.playerID === newPlayer.playerID;
+        });
     }
 
     addBetToSlip(fighterID, fighterName, odds, m: PrettyMatch) {
@@ -75,5 +97,53 @@ export class BetComponent implements OnInit {
     addBetToSlipFighter2(m: PrettyMatch) {
         this.addBetToSlip(m.fighter2ID, m.fighter2Name, m.fighter2Odds, m);
     }
-    
+
+    deleteBetFromSlip(b: PrettyBet) {
+        let index = this.betslip.indexOf(b);
+        this.betslip.splice(index, 1);
+    }
+
+    calculateToWin(b: PrettyBet) {
+        return (b.odds - 1.0) * b.stake;
+    }
+
+    getParlayOdds() {
+        if (this.betslip.length > 0) {
+
+            let allOdds = this.betslip.map(x => x.odds);
+            const reducer = (accumulator, currentValue) => accumulator * currentValue;
+            return allOdds.reduce(reducer);
+        }
+        else return 0.0;
+    }
+
+    calculateParlayToWin() {
+        let parlayOdds = this.getParlayOdds();
+        return (parlayOdds - 1.00) * this.parlayStake;
+    }
+
+    calculateTotalStake() {
+        if (this.betslip.length > 0) {
+            let allStakes = this.betslip.map(x => x.stake);
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            return allStakes.reduce(reducer);
+        }
+        else return 0.0;
+    }
+
+    calculateTotalToWin() {
+        if (this.betslip.length > 0) {
+            let allToWins = this.betslip.map(x => this.calculateToWin(x));
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            return allToWins.reduce(reducer);
+        }
+        else return 0.0;
+    }
+
+    placeBets() {
+        let totalStake = this.isParlay ? this.parlayStake : this.calculateTotalStake();
+        if (totalStake > this.selectedPlayer.currentCash) {
+            this.toastr.error('')
+        }
+    }
 }
